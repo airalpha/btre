7271f6b57d84f7e628a4d008993d25daa238b39b
AIR ALPHA <allphaair@gmail.com>
2019-11-10 21:43:54 +0100

    Base Jinja template

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(response):
-    return HttpResponse('<h1>Hello world !</h1>')
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
